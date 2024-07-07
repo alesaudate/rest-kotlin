4e25622c263068e4e542ac394f6fc0cd2a2e53ea
Alexandre <alesaudate@gmail.com>
2024-07-07 10:41:20 +0200

    spring-boot dev tools no cap 1

--- cap01/build.gradle.kts
+++ cap01/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
